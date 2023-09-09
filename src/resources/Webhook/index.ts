fd83e4a4c85200f6fc862d59cae019bcb2f4abab
stephenluc <stephenluc96@gmail.com>
2023-09-08 21:37:53 -0500

    update webhooks

--- src/resources/Webhook/index.ts
+++ src/resources/Webhook/index.ts
@@ -14,6 +14,10 @@ export const WebhookTypes = {
   payment_reversal_update: 'payment_reversal.update',
   connection_create: 'connection.create',
   connection_update: 'connection.update',
+  transaction_create: 'transaction.create',
+  transaction_update: 'transaction.update',
+  report_create: 'report.create',
+  report_update: 'report.update',
 
   // Deprecated
   account_verification_sent: 'account_verification.sent',
