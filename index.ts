f3d23f74e7bb244f33c5a95e06efc3851a428067
Michael Ossig <mrossig201@gmail.com>
2024-08-14 13:50:15 -0400

    upgrades axios version to 1.7.4, upgrades axios-retry to 4.5.0, fixes entity tests, removes incorrect deps in index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 export { Method, Method as MethodClient } from './src/method';
 export { MethodAuthorizationError, MethodInvalidRequestError, MethodInternalError } from './src/errors';
 export { Environments } from './src/configuration';
-export { AccountTypes, AccountSubTypes } from './src/resources/Account';
-export { BINBrands, BINTypes } from './src/resources/Bin';
+export { AccountTypes } from './src/resources/Account';
 export { ElementTypes } from './src/resources/Element';
-export { EntityTypes, EntityStatuses, EntityCapabilities } from './src/resources/Entity';
-export { MerchantTypes } from './src/resources/Merchant';
+export { EntityTypes, EntityStatuses } from './src/resources/Entity';
 export { PaymentFundStatuses, PaymentStatuses } from './src/resources/Payment';
 export { ReportTypes, ReportStatuses } from './src/resources/Report';
-export { RoutingNumberOfficeTypes } from './src/resources/RoutingNumber';
 export { WebhookTypes } from './src/resources/Webhook';
